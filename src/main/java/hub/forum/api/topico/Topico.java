76a7933b07b13f0388f123e33db1a71fe6d133fc
Juan Lima <juangustavo@gmail.com>
2024-05-27 19:12:21 -0300

    entidade tópico

---
+++ src/main/java/hub/forum/api/topico/Topico.java
@@ -0,0 +1,28 @@
+package hub.forum.api.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private String dataCriacao;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoTopico estadoTopico;
+
+    private String curso;
+
+}
