9ce4f905d042f982eb8fa7df31f5b6539d86f31d
Juan Lima <juangustavo@gmail.com>
2024-05-27 21:12:57 -0300

    DTO resposta

--- src/main/java/hub/forum/api/controller/RespostaController.java
+++ src/main/java/hub/forum/api/controller/RespostaController.java
@@ -1,5 +1,6 @@
 package hub.forum.api.controller;
 
+import hub.forum.api.resposta.DadosCadastroResposta;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RespostaController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroResposta dados){
+        System.out.println(dados);
     }
 
 }
---
+++ src/main/java/hub/forum/api/resposta/DadosCadastroResposta.java
@@ -0,0 +1,8 @@
+package hub.forum.api.resposta;
+
+public record DadosCadastroResposta(String mensagem,
+                                    Long topicoId,
+                                    String dataCriacao,
+                                    Long usuarioId,
+                                    boolean solucao) {
+}
