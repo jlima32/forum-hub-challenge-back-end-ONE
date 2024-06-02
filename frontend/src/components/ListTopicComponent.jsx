f556f397ab1349c27fcc1e67a96687e784a5f639
Juan Lima <juangustavo@gmail.com>
2024-06-02 15:30:32 -0300

    listando os tópicos da api

--- frontend/src/components/ListTopicComponent.jsx
+++ frontend/src/components/ListTopicComponent.jsx
@@ -1,26 +1,21 @@
+import React, {useEffect, useState} from 'react'
+
 import './ListTopicComponent.css'
+import { listTopics } from '../services/TopicService'
 
 
 const ListTopicComponent = () => {
   
+    const [topics, setTopics] = useState([])
 
-    const topicData = [
-        {
-            "id": 1,
-            "título": "Tópico 01",
-            "mensagem": "Teste 01"
-        },
-        {
-            "id": 2,
-            "título": "Tópico 02",
-            "mensagem": "Teste 02"
-        },
-        {
-            "id": 3,
-            "título": "Tópico 03",
-            "mensagem": "Teste 03"
-        }
-    ]
+    useEffect(() => {
+        listTopics().then((response) => {
+            setTopics(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+
+    }, [])
 
 
 return (
@@ -33,22 +28,23 @@ return (
                 <a href="#" className="button-new-topic">Criar Tópico</a>
         </div>
     {
-        topicData.map(topic =>
+        topics.map((topic) =>
 
             <div className="container-content" key={topic.id}>
             <div className="topic-status solved">
+
                 <i className="bx bxs-check-circle"></i>
             </div>
             <div className="topic-data">
                 <div className="topic-title">
-                    <h4><a href="">[Dúvida] Erro no Spring boot</a></h4>
+                    <h4><a href="">{topic.titulo}</a></h4>
                 </div>
                 <div className="topic-category-course">
-                    <div className="topic-category-name programacao">Programação</div>
-                    <div className="topic-course-name programacao">BDD e Java Spring Boot</div>
+                    <div className={`topic-category-name programacao ${(topic.curso.categoria).toLowerCase()}`}>{topic.curso.categoria}</div>
+                    <div className={`topic-course-name programacao ${(topic.curso.categoria).toLowerCase()}`}>{topic.curso.nome}</div>
                 </div>
                 <div className="topic-author">
-                    <p>criado por <span className="author-name">Juan Lima</span> (10 respostas)</p>
+                    <p>criado por <span className="author-name">{topic.autor.nome}</span> {`(${(topic.respostas).length} respostas)`}</p>
                 </div>
             </div>
         </div>
