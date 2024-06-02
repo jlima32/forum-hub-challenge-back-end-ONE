eec1c4229917fe410bdb136a7c21851bed25316f
Juan Lima <juangustavo@gmail.com>
2024-06-02 17:59:24 -0300

    footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListTopicComponent from './components/ListTopicComponent'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <HeaderComponent />
       <ListTopicComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FooterComponent.css'
+
+const FooterComponent = () => {
+  return (
+    <footer>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </footer>
+  )
+}
+
+export default FooterComponent
