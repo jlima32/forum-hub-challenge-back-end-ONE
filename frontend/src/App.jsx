a14bd38bf95e112221644a868e6206f6ada1375c
Juan Lima <juangustavo@gmail.com>
2024-06-03 12:32:39 -0300

    configurando rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListTopicComponent from './components/ListTopicComponent'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListTopicComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            <Route path='/' element={ <ListTopicComponent /> } ></Route>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
