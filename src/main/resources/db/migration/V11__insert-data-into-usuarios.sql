935649d6a9b27a67734e308eb4e3b5fda0ec755e
Juan Lima <juangustavo@gmail.com>
2024-06-04 19:06:22 -0300

    arquivos sql

--- src/main/resources/db/migration/V11__insert-data-into-usuarios.sql
+++ src/main/resources/db/migration/V11__insert-data-into-usuarios.sql
@@ -1,5 +1,5 @@
 INSERT INTO usuarios (nome, email, senha) VALUES
-('John Doe', 'user@email.com', '$2a$10$Hq.OwU7db.VXG8xt0Qc4dOUQHDLeu/QzQwS69kebEM1zsy19drZZS'),
-('Jane Smith', 'jane.smith@example.com', '$2a$10$gY8txi9jxOpe2seosQzTteD0LV2O1VZ/FlB4Zq1M2q9pbTy8bUBW6'),
-('Bob Johnson', 'bob.johnson@example.com', '$2a$10$7zZ4YcBqBBp3Q.vRVylcFOOoO8H.vRa4c8vGg5oT5FrfgZ5V.j6yS'),
-('Alice Williams', 'alice.williams@example.com', '$2a$10$T6sP.3cMYbSgBg7kS6Be7eBfxW.Hpxv4KT7e.2jlE2G.rKg1LP5KO');
+('John Doe', 'user@email.com', '$2a$12$fkR9FuILh/nkzOV66ar1t.csYeUEQXUOrpnLfEyLXRWDd.PyjttnG'),
+('Jane Smith', 'jane.smith@example.com', '$2a$12$Rhjnnq7LGvRjNUL3YnuLlOEYnBsiUiN44Vyti/m8lQkJMEIQkrEDS'),
+('Bob Johnson', 'bob.johnson@example.com', '$2a$12$sDkfuUlKfI4HEkP8FTLqAuSwqqOBMiw5UVtMksQSGbHP9TUnGe0Xa'),
+('Alice Williams', 'alice.williams@example.com', '$2a$12$wGJvL5QAkPnyyP5.m3YetOAyDf9a/mHVvHranXFnwMoAZulFlsk0u');
